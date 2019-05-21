4fe6bc90c326345eec64c6cacd117a280195f19a
Allan Bolipata <bolipatc@mskcc.org>
2019-05-21 16:13:33 -0400

    Minor change to run_script_lsf.sh

--- run_script_lsf.sh
+++ run_script_lsf.sh
@@ -2,6 +2,8 @@ PIPELINE_NAME=$1
 YAML_FILE=$2
 OUTPUT_NAME=$3
 
+# Make sure you have the directories `work/` and `job-stores` in your PWD
+
 toil clean job-stores/$OUTPUT_NAME
 cwltoil --singularity --logFile toil_log.log \
     --batchSystem lsf \
@@ -15,6 +17,6 @@ cwltoil --singularity --logFile toil_log.log \
     --maxMemory 256G  \
     --not-strict  \
     --realTimeLogging  \
-    --jobStore job-stores/$OUTPUT_NAME  \
-    --workDir /scratch/cwl-runs/tmp \
+    --jobStore job-stores/$OUTPUT_NAME \
+    --workDir work/ \
     --outdir $OUTPUT_NAME $PIPELINE_NAME $YAML_FILE
