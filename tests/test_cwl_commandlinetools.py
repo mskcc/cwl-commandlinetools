ccf346511cb3d93a5712575b829debf31e4c81de
rons.shah@gmail.com <rons.shah@gmail.com>
2019-10-15 22:59:16 -0400

    Chainging setup files

--- tests/test_cwl_commandlinetools.py
+++ tests/test_cwl_commandlinetools.py
@@ -6,20 +6,6 @@
 import pytest
 
 
-from cwl_commandlinetools import cwl_commandlinetools
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
+def test_content():
     """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    print("All good")
